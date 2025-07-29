3dddd7367630fa93c5f55988fef152b23b31382d
Alvi-Noman <alvinoman80@gmail.com>
2025-07-29 23:37:24 +0600

    Test configuration for auth-service(jest.config.js)

--- services/auth-service/jest.config.js
+++ services/auth-service/jest.config.js
@@ -4,5 +4,9 @@ export default {
   ...basePreset,
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^\\./routes/authRoutes\\.js$': '<rootDir>/src/routes/authRoutes.ts',
+    '^\\.{2}/controllers/authController\\.js$': '<rootDir>/src/controllers/authController.ts',
+    '^\\.{2}/server\\.js$': '<rootDir>/src/server.ts',
+    '^\\./app\\.js$': '<rootDir>/src/app.ts',
   },
 };
